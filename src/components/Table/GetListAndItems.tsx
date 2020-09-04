f5e143e01ccbafec283b5132e0a9058f3a9707bf
Spiteri <Adam.Spiteri@gov.bc.ca>
2020-09-04 08:09:37 -0700

    Added Ability to change views

--- src/components/Table/GetListAndItems.tsx
+++ src/components/Table/GetListAndItems.tsx
@@ -12,43 +12,36 @@ const getList = async (listName: string) => {
   return list;
 };
 
-const getListItems = async (listName: string) => {
-  let listItems = await GetListItems({
-    listName: listName,
-  });
-  console.log("listItems :>> ", listItems);
-  return listItems;
-};
-
-const getUserColumns = async (listName: string, list: Array<object>) => {
+const getListItems = async (listName: string, list: any) => {
   let userFieldNamesSelect: never[] = [];
   let userFieldNamesExpand = [];
   //@ts-ignore
   for (let i = 0; i < list.Fields.results.length; i++) {
     // @ts-ignore
     if (
-      // @ts-ignore
-      list.Fields.results[i].FieldTypeKind === 2 &&
-      // @ts-ignore
-      list.Fields.results[i].StaticName !== "_UIVersionString" &&
-      // @ts-ignore
-      list.Fields.results[i].StaticName !== "_CopySource"
+      !list.Fields.results[i].StaticName.startsWith("_") &&
+      list.Fields.results[i].StaticName !== "HTML_x0020_File_x0020_Type" &&
+      list.Fields.results[i].StaticName !== "Editor" &&
+      list.Fields.results[i].StaticName !== "Author" &&
+      list.Fields.results[i].StaticName !== "Edit" &&
+      list.Fields.results[i].StaticName !== "ProgId" &&
+      list.Fields.results[i].StaticName !== "SyncClientId" &&
+      list.Fields.results[i].StaticName !== "MetaInfo" &&
+      list.Fields.results[i].StaticName !== "Restricted" &&
+      list.Fields.results[i].StaticName !== "ContentVersion" &&
+      list.Fields.results[i].StaticName !== "AppAuthor" &&
+      list.Fields.results[i].StaticName !== "AppEditor" &&
+      list.Fields.results[i].StaticName !== "ProjectManager" &&
+      list.Fields.results[i].StaticName !== "BusinessLead" &&
+      list.Fields.results[i].StaticName !== "ExecutiveSponsor" &&
+      list.Fields.results[i].StaticName !== "SortBehavior" &&
+      list.Fields.results[i].StaticName !== "ScopeId" &&
+      list.Fields.results[i].StaticName !== "ItemChildCount" &&
+      list.Fields.results[i].StaticName !== "FolderChildCount"
     ) {
       //@ts-ignore
       userFieldNamesSelect.push(list.Fields.results[i].StaticName);
     }
-    //@ts-ignore
-    console.log(
-      "list.Fields.results[i].StaticName :>> ",
-      //@ts-ignore
-      list.Fields.results[i].StaticName,
-      //@ts-ignore
-      list.Fields.results[i].FieldTypeKind,
-      //@ts-ignore
-      list.Fields.results[i].TypeDisplayName,
-      //@ts-ignore
-      list.Fields.results[i].TypeAsString
-    );
   }
 
   //@ts-ignore
@@ -56,38 +49,33 @@ const getUserColumns = async (listName: string, list: Array<object>) => {
     //@ts-ignore
     if (list.Fields.results[i].FieldTypeKind === 20) {
       //@ts-ignore
-      console.log("list.Fields.results[i] :>> ", list.Fields.results[i]);
-      //@ts-ignore
       userFieldNamesSelect.push(list.Fields.results[i].StaticName + "/Title");
       //@ts-ignore
       userFieldNamesExpand.push(list.Fields.results[i].StaticName);
     }
   }
 
-  console.log(" userFieldNames :>> ", userFieldNamesSelect);
-
-  let listUserColumns = await GetListItems({
+  let items = await GetListItems({
     listName: listName,
     expand: userFieldNamesExpand,
     select: userFieldNamesSelect,
   });
-  console.log("listItems :>> ", listUserColumns);
-  return listUserColumns;
+  // console.log("items :>> ", items);
+  return items;
 };
 
 export const getListAndItems = async (listName: string) => {
-  let title, columns, items, views, list, listUserColumns;
+  let title, columns, views, list: any, items, viewColumns;
   list = await getList(listName);
-  items = await getListItems(listName);
-  listUserColumns = await getUserColumns(listName, list);
+  items = await getListItems(listName, list);
   views = list.Views.results.map((view: Object) => {
     //@ts-ignore
     return { title: view.Title, fields: view.ViewFields.Items.results };
   });
-  console.log("views :>> ", views);
+  // console.log("views :>> ", views);
   title = list.Title;
-
-  let listColumns = {};
+  // returns the Fields Titles and Types
+  let listColumns: any = {};
   for (let i = 0; i < list.Fields.results.length; i++) {
     //@ts-ignore
     listColumns[list.Fields.results[i].InternalName] = {
@@ -95,11 +83,11 @@ export const getListAndItems = async (listName: string) => {
       FieldTypeKind: list.Fields.results[i].FieldTypeKind,
     };
   }
-
+  // console.log("listColumns :>> ", listColumns);
   //Table Columns
-
   columns = list.DefaultView.ViewFields.Items.results.map((field: string) => {
-    let fieldObject = {
+    // console.log("field :>> ", field);
+    let fieldObject: any = {
       //@ts-ignore
       title: listColumns[field].Title,
       field: field,
@@ -129,16 +117,83 @@ export const getListAndItems = async (listName: string) => {
           />
         );
       };
+    } else if (listColumns[field].FieldTypeKind === 20) {
+      fieldObject.render = (rowdata: any) => {
+        // console.log("rowdata", rowdata);
+        return <div>{rowdata.ProjectManager.Title}</div>;
+      };
     }
 
     if (field === "LinkTitle") {
       //@ts-ignore
       fieldObject.render = (rowdata) => {
-        return <a href={rowdata.File.ServerRelativeUrl}>{rowdata.Title}</a>;
+        return <a href={rowdata.FileDirRef + "/" + rowdata.ProjectName}>{rowdata.Title}</a>;
       };
     }
     return fieldObject;
   });
+  console.log("fieldObject :>> ", columns);
 
-  return { title, columns, items, views };
+  let viewsObject: any = [];
+  for (let i = 0; i < list.Views.results.length; i++) {
+    viewsObject.push({
+      viewTitle: list.Views.results[i].Title,
+      fields: list.Views.results[i].ViewFields.Items.results.map((field: string) => {
+        // console.log("field :>> ", field);
+        let viewsObject: any = {
+          //@ts-ignore
+          title: listColumns[field].Title,
+          field: field,
+        };
+        //@ts-ignore
+        if (listColumns[field].FieldTypeKind === 4) {
+          //datetime
+          //@ts-ignore
+          viewsObject.render = (rowdata) => {
+            return (
+              <Moment fromNowDuring={3600000} format={"dddd, MMMM Do, YYYY @ h:mm a"}>
+                {rowdata[field]}
+              </Moment>
+            );
+          };
+        } else if (
+          //@ts-ignore
+          listColumns[field].FieldTypeKind === 3 //multilinetext
+        ) {
+          //@ts-ignore
+          viewsObject.render = (rowdata) => {
+            return (
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: rowdata[field],
+                }}
+              />
+            );
+          };
+        } else if (listColumns[field].FieldTypeKind === 20) {
+          viewsObject.render = (rowdata: any) => {
+            // console.log("rowdata", rowdata);
+            return <div>{rowdata.ProjectManager.Title}</div>;
+          };
+        }
+
+        if (field === "LinkTitle") {
+          //@ts-ignore
+          viewsObject.render = (rowdata) => {
+            return <a href={rowdata.FileDirRef + "/" + rowdata.ProjectName}>{rowdata.Title}</a>;
+          };
+        }
+        return viewsObject;
+      }),
+    });
+  }
+
+  console.log("viewsObject :>> ", viewsObject);
+
+  // viewColumns = list.Views.results.map((views: string) => {
+  //   viewsObject.push({ title: list.Views.results.ViewFields.Items.results, field: list.Views.results.Title });
+  //   console.log("viewsObject :>> ", viewsObject);
+  // });
+
+  return { title, columns, views, items, viewsObject };
 };
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,6 +3,8 @@ import MaterialTable, { MTableToolbar } from "material-table";
 import { getListAndItems } from "Components";
 import { Icons } from "Components";
 import { ListSubheader } from "@material-ui/core";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 interface TableProps {
   listName: string;
@@ -19,11 +21,24 @@ export const Table: FC<TableProps> = ({ listName, options, tableTitle }) => {
   const [actions, setActions] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [components, setComponents] = useState();
+  const [view, setView] = useState();
 
   const populateTable = async () => {
     setIsLoading(true);
 
     const list = await getListAndItems(listName);
+
+    // console.log("list.columns :>> ", list.columns);
+
+    const handleViewChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+      setView(event.target.value);
+      for (let i = 0; i < list.views.length; i++) {
+        if (event.target.value === list.viewsObject[i].viewTitle) {
+          setColumns(list.viewsObject[i].fields);
+          console.log("list.viewsObject[i].fields :>> ", list.viewsObject[i].fields);
+        }
+      }
+    };
 
     if (tableTitle) {
       setTitle(tableTitle);
@@ -36,14 +51,17 @@ export const Table: FC<TableProps> = ({ listName, options, tableTitle }) => {
 
     setIsLoading(false);
 
-    console.log("listFromPopTable :>> ", list);
+    // console.log("listFromPopTable :>> ", list);
+    // console.log("items", list.items);
     const x = {
       Toolbar: (props: any) => (
         <div>
+          <Select id="demo-simple-select-outlined" value={view} onChange={handleViewChange} label="Views">
+            {list.views.map((view: any) => {
+              return <MenuItem value={view.title}>{view.title}</MenuItem>;
+            })}
+          </Select>
           <MTableToolbar {...props} />
-          {list.views.map((view: any) => {
-            return view.title;
-          })}
         </div>
       ),
     };
